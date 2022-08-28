2a2599b167b593807a5594c4dbe331795877f207
DaZiYuan <mscoder@hotmail.com>
2022-08-29 01:31:21 +0800

    staticFiles

--- app.ts
+++ app.ts
@@ -1,13 +1,13 @@
-import { Router, Application } from "./deps.ts";
+import { Router, Application, staticFiles } from "./deps.ts";
 
 const router = new Router();
 router
-  .get("/", async (context) => {
-    await context.send({
-      root: `${Deno.cwd()}\\static`,
-      index: "index.html",
-    });
-  })
+  // .get("/", async (context) => {
+  //   await context.send({
+  //     root: `${Deno.cwd()}\\static`,
+  //     index: "index.html",
+  //   });
+  // })
   .get("/api/baidu/:json", (context) => {
     if (context.params && context.params.json) {
       context.response.body = context.params.json + "2";
@@ -17,5 +17,6 @@ router
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(staticFiles("public"));
 
 await app.listen({ port: 8000 });
