4bc21c84ec638822b2df901c146289adb24aa38a
DaZiYuan <mscoder@hotmail.com>
2022-08-29 00:32:52 +0800

    渲染html

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ json 在线翻译
 # 翻译 api 文档
 
 https://fanyi-api.baidu.com/doc/21
+
+# 命令
+
+```
+denon start
+```
---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import { Router, Application } from "./deps.ts";
+
+const router = new Router();
+router
+  .get("/", async (context) => {
+    await context.send({
+      root: `${Deno.cwd()}\\src\\static`,
+      index: "index.html",
+    });
+  })
+  .get("/api/baidu/:json", (context) => {
+    if (context.params && context.params.json) {
+      context.response.body = context.params.json + "2";
+    }
+  });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
